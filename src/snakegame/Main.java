51689361798f82d6f2f344088ef66880f66a5f67
leonljh <leonlowjunhao@hotmail.com>
2020-10-10 22:59:52 +0800

    Snake Logic

--- src/snakegame/Main.java
+++ src/snakegame/Main.java
@@ -1,6 +1,25 @@
 package snakegame;
 
+import snakegame.domain.Snake;
+
 public class Main {
     public static void main(String[] args) {
+        Snake worm = new Snake(5, 5, Direction.RIGHT);
+        System.out.println(worm.getPieces()); // 5,5
+        worm.move();
+        System.out.println(worm.getPieces()); //5,5 6,5
+        worm.move();
+        System.out.println(worm.getPieces()); // [(5,5), (6,5), (7,5)]
+        worm.move();
+        System.out.println(worm.getPieces()); //[(6,5), (7,5), (8,5)]
+        worm.grow();
+        System.out.println(worm.getPieces()); //[(6,5), (7,5), (8,5)]
+        worm.move();
+        System.out.println(worm.getPieces());  //[(6,5), (7,5), (8,5), (9,5)]
+
+        worm.setDirection(Direction.LEFT);
+        System.out.println(worm.runsIntoItself());
+        worm.move();
+        System.out.println(worm.runsIntoItself());
     }
 }
--- src/snakegame/domain/Snake.java
+++ src/snakegame/domain/Snake.java
@@ -9,7 +9,9 @@ public class Snake {
     private int x;
     private int y;
     private Direction dir;
-    private List<Piece> length;
+    private List<Piece> snake;
+    private boolean growin;
+
 
 
     public Snake(int originalX, int originalY, Direction originalDirection){
@@ -17,8 +19,9 @@ public class Snake {
         this.y = originalY;
         this.dir = originalDirection;
         Piece piece = new Piece(this.x,this.y);
-        length = new ArrayList<Piece>();
-        length.add(piece);
+        snake = new ArrayList<Piece>();
+        snake.add(piece);
+        growin = false;
     }
 
     public Direction getDirection(){
@@ -30,26 +33,73 @@ public class Snake {
     }
 
     public int getLength(){
-        return length.size();
+        return snake.size();
     }
 
     public List<Piece> getPieces(){
-        return this.length;
+        return this.snake;
     }
 
     public void move(){
+        //first 3 moves will implement grow
+        //after that it just moves
 
+        if(getLength() < 3){
+            growin = true;
+        }
+
+        Piece p = null;
+
+        if(getDirection() == Direction.LEFT){
+            p = new Piece(x-1,y);
+            x -= 1;
+        }
+        if(getDirection() == Direction.RIGHT) {
+            p = new Piece(x+1,y);
+            x += 1;
+        }
+        if(getDirection() == Direction.UP) {
+            p = new Piece(x,y+1);
+            y += 1;
+        }
+        if(getDirection() == Direction.DOWN){
+            p = new Piece(x,y-1);
+            y -= 1;
+        }
+
+        snake.add(p);
+
+        if(!growin){
+            snake.remove(0);
+        }
+        else{
+           growin = false;
+        }
     }
 
     public void grow(){
-
+        growin = true;
     }
 
     public boolean runsInto(Piece piece){
-
+        if(piece.getX() == this.x && piece.getY() == this.y){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    public boolean runsIntoItself(){
+    public boolean runsIntoItself() {
+        //first find location of head.
+        //if head location and set direction leads to a clash, run true;
+        //else run false
 
+        for (int i = 0; i < snake.size()-1; i++) {
+            if (this.x == snake.get(i).getX() && this.y == snake.get(i).getY()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
